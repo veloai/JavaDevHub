890a97c80354bacb06793170610f5db548a4d18f
wjson <wjson@modeal.net>
2023-07-20 17:12:07 +0900

    Refactor: AppConfig

--- security/src/main/java/dev/hub/security/config/AppConfing.java
+++ security/src/main/java/dev/hub/security/config/AppConfing.java
@@ -1,13 +1,16 @@
 package dev.hub.security.config;
 
+import dev.hub.security.system.AppIntercepter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class AppConfing {
+public class AppConfing implements WebMvcConfigurer {
 
     private final AppIntercepter appIntercepter;
 
